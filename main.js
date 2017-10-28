73a5dd1a77ffce705d39e3338e26b6d563be243f
kevin578 <kbriggs@MA000894-KBRIGGS.hsd1.ct.comcast.net>
2017-10-28 09:25:54 -0400

    changed JSON package

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const moment = require('moment')
+const moment = require('moment');
+const fs = require('fs');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
+
+
+app.get('/', function(req, res) {
+  fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    res.write(html);
+    res.end();
+  });
+});
+
 
 
 app.get('/:tagId', function(req, res) {
@@ -9,6 +23,7 @@ app.get('/:tagId', function(req, res) {
 
   var unix_time = new RegExp(/\d{10}/)
   var natural_time = new RegExp(/(January|February|March|April|May|June?|July|August|September|October|November|December)\s(\d\d?).+?(\d\d\d\d)/)
+
   if (unix_time.test(req.params.tagId)){
 
       res.json({
@@ -19,7 +34,7 @@ app.get('/:tagId', function(req, res) {
 
   }
   else if (natural_time.test(req.params.tagId)) {
-      res.send({
+      res.json({
         unix: moment("January 31 1984").format("X"),
         natural: moment(req.params.tagId).format("dddd, MMMM Do YYYY")
       })
@@ -35,6 +50,6 @@ app.get('/:tagId', function(req, res) {
 
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(app.get('port'), function () {
   console.log('Server started')
 });
